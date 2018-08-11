47224e1b68bc9189965804f08d9bbd9ac5a53914
LASP-UCL <40371677+LASP-UCL@users.noreply.github.com>
2018-08-11 22:03:07 +0100

    Add files via upload

--- primal_dual_gl.py
+++ primal_dual_gl.py
@@ -20,7 +20,8 @@ from synthetic_data import *
 from scipy.optimize import minimize
 
 
-def ssf(n):
+def sum_squareform(n):
+	#sum operator that find degree from upper triangle
 	ncols=(n-1)*n/2
 	I=np.zeros(ncols)
 	J=np.zeros(ncols)
@@ -66,12 +67,35 @@ def vector_form(W,n):
 
 
 class Primal_dual_gl():
-	def __init__(self, z, alpha, beta, w_0, d_0, gamma, eplison, iteration):
-		self.z=z
+	def __init__(self, node_num, Z, alpha, beta, w_0, d_0, gamma, eplison, iteration):
+		self.node_num=node_num
+		self.Z=Z
+		self.z=vector_form(Z)
 		self.alpha=alpha
 		self.beta=beta
-		self.w_0=w_0
-		self.d_0=d_0
+		self.w=w_0
+		self.d=d_0
+		self.S=sum_squareform(node_num)
 		self.gamma=gamma
 		self.eplison=eplison
 		self.iteration=iteration
+
+	def run(self):
+		for i in range(iteration):
+			y=self.w-self.gamma*(2*self.beta*self.w+np.dot(self.S.T, self.d))
+			y_bar=self.d+self.gamma*(np.dot(self.S, self.w))
+			p=np.max([0, y-2*self.gamma*self.z])
+			p_bar=(y_bar-np.sqrt((y_bar)**2+4*self.alpha*self.gamma))/2.0
+			q=p-self.gamma*(2*self.beta*p+np.dot(self.S.T,p))
+			q_bar=p_bar+self.gamma*(np.dot(self.S, p))
+			w_i_1=self.w
+			self.w=self.w-y+p
+			d_i_1=self.d
+			self.d=self.d-y_bar+q_bar
+			w_diff=np.linalg.norm(self.w-w_i_1)/np.linalg.norm(w_i_1)
+			d_diff=np.linalg.norm(self.d-d_i_1)/np.linalg.norm(d_i_1)
+			if (w_diff<self.eplison) and (d_diff<self.eplison):
+				break 
+			else:
+				pass 
+		return self.w
